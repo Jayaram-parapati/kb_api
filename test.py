8ea538c843423a58bf36dd5efa495af97508ebdd
Jayaram-parapati <140514791+Jayaram-parapati@users.noreply.github.com>
2024-01-02 12:24:46 +0530

    ro02/01/2024 1202tg

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import os 
+from datetime import datetime
+import time
 
 print("Hello World")
 print("curren directory : ",os.getcwd())
 print("first GITHUB ACTION")
 print("test.py found")
+while True:
+    print(datetime.now())
+    time.sleep(1)
